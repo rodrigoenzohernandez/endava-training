ffe5acc4c99c5b7435b6f75a3a8e26a66cb2de99
Rodrigo Hernandez <rodrigoenzohernandez@gmail.com>
2021-11-15 16:48:00 -0300

    add exercise-16

---
+++ 14-js-basics/exercise-16.js
@@ -0,0 +1,8 @@
+/**
+ * Let's get some practice defining constants. Please define the following two variables: 
+ * Define a constant called boilingPointC and set it to 100
+ * Fedine a second constant called boilingPointF and set it to 212
+ */
+
+const boilingPointC = 100;
+const boilingPointF = 212;
