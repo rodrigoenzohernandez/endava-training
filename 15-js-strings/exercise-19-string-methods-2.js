160cf40469ac97822add8055665a17d58d65cafd
Rodrigo Hernandez <rodrigoenzohernandez@gmail.com>
2021-11-16 10:56:48 -0300

    add exercise 19

---
+++ 15-js-strings/exercise-19-string-methods-2.js
@@ -0,0 +1,18 @@
+/**
+ * I've provided you with a word variable, set to "skateboard". 
+ * Your goal is to use string methods on word, so that you end up with the string "beard"
+ * 1- Use a string method to extract the "board" part of "skateboard"  (using the slice method)
+ * 2- Replace the "o" in board with an "e" (using the replace method)
+ * 3- Save the result in a variable called facialHair
+ * 
+ * This is another exercise that is very easy to cheat! I can't verify that you actually call the correct string methods.  
+ * You could just copy and paste the answer into the variable. 
+ * 
+ *  BUT IF YOU DO THAT YOU WON'T BE ABLE TO LIVE WITH THE GUILT.  IT WILL TEAR YOU APART FOR AS LONG AS YOU LIVE! 
+ * 
+ * So, please follow the spirit of the exercise :) 
+ */
+
+ const word = "skateboard"; //Don't change this line!
+
+ let facialHair = word.slice(5).replace('o','e')
