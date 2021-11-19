1dd520c72da218a4f4f51f506f48010d40616aea
Rodrigo Hernandez <rodrigoenzohernandez@gmail.com>
2021-11-19 12:11:06 -0300

    add exercise 44

---
+++ 21-js-functions/exercise-44-function-expression.js
@@ -0,0 +1,13 @@
+/**
+ * Define a function that returns the square of a number. Define the function as a function
+expression, stored in a variable called square. NOTE: Udemy's coding exercise platform
+does NOT support the ** operator.
+          square(4) //16
+          square(3) //9
+ */
+
+const square = function (number) {
+    return number * number
+}
+
+console.log(square(10));
